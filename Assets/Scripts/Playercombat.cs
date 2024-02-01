96dc578d2afa06c151b7506dcf18aed9e39462f6
Krillecummings <kristoffer.cummings@skola.taby.se>
2024-02-01 08:42:16 +0100

    tog bort merge conflicts

--- Assets/Scripts/Playercombat.cs
+++ Assets/Scripts/Playercombat.cs
@@ -1,39 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Playercombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    public Transform attackPoint;
+   
 
-    public Animator animator;
-
-    public float attackRange = 0.5f;
-
-    public LayerMask enemyLayers;
+    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.Space)) ;
         {
             Attack();
         }
     }
     void Attack()
     {
-        animator.SetTrigger("Attack");
-
-        Collider2D[] hitEnemies= Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-
-        foreach(Collider2D collider in hitEnemies)
-        {
-            Debug.Log("Träff");
-        }
+        
     }
-  
 }
